4b05903ebba70ca1cfcb7708636e092220024752
Eugene <eugenelitvinko@gmail.com>
2023-05-31 07:57:39 +0300

    add first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Задача 1. Представлен список из значений "Xiaomi Redmi Note 10S", "Смартфон Xiaomi Redmi Note 10 Pro",
+#"Apple iPhone 13", "Apple iPhone 11", "Huawei nova Y70", "Смартфон Apple iPhone 13 Pro".
+#Необходимо создать новый список, содержащий модели бренда Apple.
+
+
+current_list = ["Xiaomi Redmi Note 10S", "Смартфон Xiaomi Redmi Note 10 Pro", "Apple iPhone 13", "Apple iPhone 11", "Huawei nova Y70", "Смартфон Apple iPhone 13 Pro"]
+list_apple = []
+def creat_list_from_apple():
+    for i in range(0, len(current_list)):
+        elem_from_cur_list = current_list[i]
+        string_from_elem = str(elem_from_cur_list).split()
+        if string_from_elem.count('Apple') == 1:
+            list_apple.append(current_list[i])
+    if list_apple: return print(list_apple)
+    else: return print("Моделей бренда Apple в текущем списке нет")
+
+if __name__ == '__main__':
+    creat_list_from_apple()
+
